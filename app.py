0c355078690d79976afc383956d10a31ffd14c3c
hariniprakash296 <hariniprakash296@gmail.com>
2023-06-22 18:25:14 +0800

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello Bitch!'
+def index():
+    '''
+    Displays the index page accessible at '/'
+    '''
+    
+    return render_template('index.html')
+
+@app.route("/liveChartPrediction")
+def liveChartPrediction():
+    '''
+    Displays the index page accessible at '/'
+    '''
+    return render_template('liveChartPrediction.html')
+
+@app.route('/liveChart')
+def liveChart():
+    return render_template('liveChart.php')
 
 if __name__ == '__main__':
     app.run(debug=True)
